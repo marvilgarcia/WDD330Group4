86528601ff80fd232d6666b953a9b76d5c27f10a
marvilgarcia <marvigarcia015@gmail.com>
2026-08-01 11:51:28 -0400

    import error because of product details name is lower case

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import ProductDetails from "./productDetails.mjs";
 
 loadHeaderFooter();
 
